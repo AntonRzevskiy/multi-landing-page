947f9d7c0c457087a1328e9cc50c54efd8afc1d2
Anton <31966248+AntonRzevskiy@users.noreply.github.com>
2018-12-12 14:18:50 +0200

    remove query & add getters

--- includes/core/abstract-class-mlp-query-base.php
+++ includes/core/abstract-class-mlp-query-base.php
@@ -77,84 +77,6 @@ abstract class MLP_Query_Base {
 	/**
 	 * .
 	 *
-	 * @since    1.0.0
-	 *
-	 * @param      bool           $strict        Optional. TRUE - This means that the result of the query
-	 *                                           will not be processed additionally. FALSE - Postprocessing
-	 *                                           is assumed. Also, all tracks get information about this value.
-	 *                                           Default true.
-	 */
-	public function query( $query_args = array(), $url = array(), $strict = true ) {
-
-		$this->strict = $strict;
-
-		$this->query_args = wp_parse_args( $query_args );
-
-		// parse URL
-		$this->url = wp_parse_args( $url, $_GET );
-
-		/**
-		 * .
-		 *
-		 * @since      1.0.0
-		 *
-		 * @param      array          $url           .
-		 */
-		$this->url = apply_filters( 'mlp_url', $this->url );
-
-
-		$post_type = isset( $this->query_args[ 'post_type' ] ) ? $this->query_args[ 'post_type' ] : 'any';
-
-		$tax_query = isset( $this->query_args[ 'tax_query' ] ) ? $this->query_args[ 'tax_query' ] : array();
-
-		$meta_query = isset( $this->query_args[ 'meta_query' ] ) ? $this->query_args[ 'meta_query' ] : array();
-
-
-		$registered_tax_tracks = $this->registry->get_taxonomy_tracks( $post_type );
-
-		$white_tax_tracks = $this->get_white_list_tracks( $registered_tax_tracks, $this->url );
-
-		$this->query_args[ 'tax_query' ] = $this->prepare_tax_query( $tax_query, $white_tax_tracks );
-
-
-		$registered_meta_tracks = $this->registry->get_meta_tracks( $post_type );
-
-		$white_meta_tracks = $this->get_white_list_tracks( $registered_meta_tracks, $this->url );
-
-		$this->query_args[ 'meta_query' ] = $this->prepare_meta_query( $meta_query, $white_meta_tracks );
-
-		/**
-		 * .
-		 *
-		 * @since      1.0.0
-		 *
-		 * @param      array          $query_args    .
-		 */
-		$this->query_args = apply_filters( 'mlp_query', $this->query_args );
-
-		$this->last_query = new WP_Query( $this->query_args );
-
-
-		if ( false === $this->strict ) {
-
-			/**
-			 * .
-			 *
-			 * @since      1.0.0
-			 *
-			 * @param      object         $this          .
-			 */
-			do_action_ref_array( 'mlp_query_post_processing', array( &$this ) );
-
-		}
-
-
-		return $this->last_query;
-	}
-
-	/**
-	 * .
-	 *
 	 * @since      1.0.0
 	 *
 	 * @access     protected
@@ -237,6 +159,39 @@ abstract class MLP_Query_Base {
 		}
 
 		return $out;
+	}
+
+	/**
+	 * .
+	 *
+	 * @since      1.0.0
+	 *
+	 * @return     bool           .
+	 */
+	public function is_strict() {
+		return (bool) $this->strict;
+	}
+
+	/**
+	 * .
+	 *
+	 * @since      1.0.0
+	 *
+	 * @return     array          .
+	 */
+	public function get_query_args() {
+		return $this->query_args;
+	}
+
+	/**
+	 * .
+	 *
+	 * @since      1.0.0
+	 *
+	 * @return     array          .
+	 */
+	public function get_url() {
+		return $this->url;
 	}
 
 
